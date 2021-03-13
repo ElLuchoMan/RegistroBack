5623750943a99c12e6ba5e42b9cc9c5684570930
ElLuchoMan <luchoman96@gmail.com>
2021-03-12 21:42:26 -0500

    Se agrega modelo registro

---
+++ registro/Models/Registro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace registro.Models
+{
+    public class Registro
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string TipoDocumento { get; set; }
+        public int Documento { get; set; }
+        public int Telefono { get; set; }
+
+    }
+}
