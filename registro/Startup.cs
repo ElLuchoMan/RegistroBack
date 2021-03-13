380d30e75f775dcdd89e0dd38c9b8d3a3fd64711
ElLuchoMan <luchoman96@gmail.com>
2021-03-12 23:54:47 -0500

    Se agrega cors a startup

--- registro/Startup.cs
+++ registro/Startup.cs
@@ -36,6 +36,13 @@ namespace registro
             string mySqlConnection = Configuration.GetConnectionString("Default");
             services.AddDbContext<AplicationDBContext>(options =>
             options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
