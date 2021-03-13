4f14dd9235d38c6a9a001a0f2aaac58bd3e5530d
ElLuchoMan <luchoman96@gmail.com>
2021-03-12 21:54:37 -0500

    Se completa contexto

--- registro/AplicationDBContext.cs
+++ registro/AplicationDBContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using registro.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace registro
 {
-    public class AplicationDBContext
+    public class AplicationDBContext : DbContext
     {
+        public DbSet<Registro> Registro { get; set; }
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options)
+        {
+
+        }
     }
 }
