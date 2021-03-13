e22884b0f03d2359aee86c80f30230171b2f2cd1
ElLuchoMan <luchoman96@gmail.com>
2021-03-12 21:45:05 -0500

    Se completa modelo registro

--- registro/Models/Registro.cs
+++ registro/Models/Registro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,15 @@ namespace registro.Models
     public class Registro
     {
         public int id { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Correo { get; set; }
+        [Required]
         public string TipoDocumento { get; set; }
+        [Required]
         public int Documento { get; set; }
+        [Required]
         public int Telefono { get; set; }
 
     }
