8d75657c1c3257360f32e6819e8578a6371ee171
ElLuchoMan <luchoman96@gmail.com>
2021-03-12 22:34:13 -0500

    Se genera migración

---
+++ registro/Migrations/20210313033052_v1.0.0.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace registro.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registro",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Correo = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    TipoDocumento = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Documento = table.Column<int>(type: "int", nullable: false),
+                    Telefono = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registro", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registro");
+        }
+    }
+}
